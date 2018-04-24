56d2c5b1432355dc7ded88bba54406de0c3adef3
kottai <rajkottai41@gmail.com>
2018-04-24 06:57:02 +0530

    Done

--- src/main/java/Springmain/controller/DisplayController.java
+++ src/main/java/Springmain/controller/DisplayController.java
@@ -23,11 +23,18 @@ public List<Display> getInfo(){
 }
 @GetMapping("/display/{id}")
 public Display getperinfor(@PathVariable String id){
-	return list.stream().filter(d -> d.getName().equals(id)).findFirst().get();
+	return list.stream().filter(d -> d.getId().equals(id)).findFirst().get();
 }
 @RequestMapping(method=RequestMethod.POST,value="/display")
 public void addData(@RequestBody Display display){
 	System.out.println("the json data"+display);
 	list.add(display);
 }
+//**************Update data******************
+@RequestMapping(method=RequestMethod.PUT,value="/display/{id}")
+public void update(@RequestBody Display display,@PathVariable String id){
+	list.add(display);
+	System.out.println("Successfully updated");
+}
+
 }
